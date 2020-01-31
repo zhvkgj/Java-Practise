6841265ecc4cd69a5ec41231d6d72d9925958c86
zhvkgj <zhvkgj@yandex.ru>
2020-01-31 21:27:50 +0500

    add max stairs sum bottom-up task

--- algortihms-methods/src/dynamic_programming/StairsSumBU.java
+++ algortihms-methods/src/dynamic_programming/StairsSumBU.java
@@ -1,6 +1,5 @@
 package dynamic_programming;
 
-import javax.swing.plaf.metal.MetalIconFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -13,8 +12,19 @@ public class StairsSumBU {
         for (int i = 0; i < n; i++) {
             stairs[i] = scan.nextInt();
         }
-
-        
+        int prevPrev = 0;
+        int prev = stairs[0];
+        if (n == 1) {
+            System.out.println(prev + prevPrev);
+            return;
+        }
+        int curr = 0;
+        for (int i = 1; i < n; i++) {
+            curr = stairs[i] + Math.max(prevPrev, prev);
+            prevPrev = prev;
+            prev = curr;
+        }
+        System.out.println(curr);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
